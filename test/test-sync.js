8348d405d8d9b5d4ec8dcbd50c0f8bc2e26289c3
Karl Skomski <karl@skomski.com>
2012-06-08 19:23:55 +0200

    Better error handling

--- test/test-sync.js
+++ test/test-sync.js
@@ -8,6 +8,50 @@ var testKey = crypto.randomBytes(32);
 var testIv  = crypto.randomBytes(16);
 var testBuffer = fs.readFileSync(__dirname + '/urls.10K');
 
+assert.throws(
+  function() {
+    sosemanuk.createCipherSync();
+  },
+  /`key` != type\(Buffer\)/
+);
+
+assert.throws(
+  function() {
+    sosemanuk.createCipherSync(new Buffer('2'));
+  },
+  /`key` != \(16 >= && <= 32 bytes\)/
+);
+
+assert.throws(
+  function() {
+    sosemanuk.createCipherSync(testKey);
+  },
+  /`iv` != type\(Buffer\)/
+);
+
+assert.throws(
+  function() {
+    sosemanuk.createCipherSync(testKey, new Buffer('2'));
+  },
+  /`iv` != \(16 >= && <= 32 bytes\)/
+);
+
+assert.throws(
+  function() {
+    var cipher = sosemanuk.createCipherSync(testKey, testIv);
+    cipher.encryptSync();
+  },
+  /args.length != 1/
+);
+
+assert.throws(
+  function() {
+    var cipher = sosemanuk.createCipherSync(testKey, testIv);
+    cipher.encryptSync('2');
+  },
+  /args\[0\] != type\(Buffer\)/
+);
+
 var cipher = sosemanuk.createCipherSync(testKey, testIv);
 var encrypted = cipher.encryptSync(testBuffer);
 
